08c07ff13c9f825152afaf2677b69b44ce218dd8
Dark_Loon <armandgaeln@gmail.com>
2025-10-27 21:58:52 +0100

    Add jq to work with bitwarden-cli and comment out claude-code

--- home.nix
+++ home.nix
@@ -1,41 +1,41 @@
-{ config, pkgs, ... }:
-{
-   home.username = "armand";
-   home.homeDirectory = "/home/armand";
-   home.stateVersion = "25.05";
+{ config, pkgs, ... }: {
+  home.username = "armand";
+  home.homeDirectory = "/home/armand";
+  home.stateVersion = "25.05";
 
-   home.packages = with pkgs; [
-     tor-browser-bundle-bin # Privacy-focused browser routing traffic through the Tor network
-     firefox
-     # mullvad # Mullvad VPN command-line client tools
-     # qbittorrent # OpenSource Qt Bittorrent client
-     yt-dlp # Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)
-     vlc # Media Player
-     # dim # Self-hosted media manager
-     # zellij # Terminal workspace with batteries included
-     # niri # Scrollable-tiling Wayland compositor
-     rucola # Terminal-based markdown note manager
-     zathura # zathura
-     nushell # Modern shell written in Rust
-     ghostty # Fast, native, feature-rich terminal emulator pushing modern features
-     yazi # Terminal file manager
-     claude-code # Agentic coding tool
-     bitwarden-cli # Secure and free password manager for all of your devices
-     wiki-tui # Simple and easy to use Wikipedia Text User Interface
-     # hledger # CLI for the hledger accounting system 
-   ]; 
+  home.packages = with pkgs; [
+    tor-browser-bundle-bin # Privacy-focused browser routing traffic through the Tor network
+    firefox
+    # mullvad # Mullvad VPN command-line client tools
+    # qbittorrent # OpenSource Qt Bittorrent client
+    yt-dlp # Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)
+    vlc # Media Player
+    # dim # Self-hosted media manager
+    # zellij # Terminal workspace with batteries included
+    # niri # Scrollable-tiling Wayland compositor
+    jq
+    rucola # Terminal-based markdown note manager
+    zathura # zathura
+    nushell # Modern shell written in Rust
+    ghostty # Fast, native, feature-rich terminal emulator pushing modern features
+    yazi # Terminal file manager
+    # claude-code # Agentic coding tool
+    bitwarden-cli # Secure and free password manager for all of your devices
+    wiki-tui # Simple and easy to use Wikipedia Text User Interface
+    # hledger # CLI for the hledger accounting system 
+  ];
 
-   programs.git = {
-     enable = true;
-     userName = "Dark_Loon";
-     userEmail = "armandgaeln@gmail.com";
-     aliases = {
-       co = "checkout";
-       st = "status";
-     };
-   };
+  programs.git = {
+    enable = true;
+    userName = "Dark_Loon";
+    userEmail = "armandgaeln@gmail.com";
+    aliases = {
+      co = "checkout";
+      st = "status";
+    };
+  };
 
-   programs.helix = {
+  programs.helix = {
     enable = true;
     settings = {
       theme = "autumn_night_transparent";
