c89e7711b02926e9c85000362f7419899837f702
Dark_Loon <armandgaeln@gmail.com>
2026-01-24 02:17:45 +0100

    Update Nushell config - exit CWD on exit with q - y to start instead of yazi - the default shell - welcome message disabled

--- home/programs/nushell/nushell.nix
+++ home/programs/nushell/nushell.nix
@@ -2,46 +2,32 @@
 {
   programs.nushell = {
     enable = true;
-    settings = {
-      buffer_editor = "hx";
-      show_banner = false;
-      completions = {
-        case_sensitive = false; # case-sensitive completions
-        quick = true; # set to false to prevent auto-selecting completions
-        partial = true; # set to false to prevent partial filling of the prompt
-        algorithm = "fuzzy"; # prefix or fuzzy
-        external = {
-          enable = true;
-          max_results = 100;
-          completer = "$multiple_completers";
-        };
-      };
-    };
     extraConfig = ''
-      def --env y [...args] {
-      	let tmp = (mktemp -t "yazi-cwd.XXXXXX")
-      	^yazi ...$args --cwd-file $tmp
-      	let cwd = (open $tmp)
-      	if $cwd != $env.PWD and ($cwd | path exists) {
-      		cd $cwd
-      	}
-      	rm -fp $tmp
-      }
+      # Set config values directly
+      $env.config.buffer_editor = "hx"
+      $env.config.show_banner = false
+      $env.config.completions.case_sensitive = false
+      $env.config.completions.quick = true
+      $env.config.completions.partial = true
+      $env.config.completions.algorithm = "fuzzy"
+      $env.config.completions.external.enable = true
+      $env.config.completions.external.max_results = 100
 
-      let carapace_completer = {{{!}}spans: list&lt;string&gt;{{!}}
+      # Define completers BEFORE referencing them
+      let carapace_completer = {|spans: list<string>|
           carapace $spans.0 nushell ...$spans
           | from json
-          | if ($in | default [] {{!}} where value == $"($spans {{!}} last)ERR" {{!}} is-empty) { $in } else { null }
+          | if ($in | default [] | where value == $"($spans | last)ERR" | is-empty) { $in } else { null }
       }
 
       $env.CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense'
 
-      let zoxide_completer = {{{!}}spans{{!}}
-            $spans {{!}} skip 1 {{!}} zoxide query -l ...$in {{!}} lines {{!}} where {{{!}}x{{!}} $x != $env.PWD}
+      let zoxide_completer = {|spans|
+          $spans | skip 1 | zoxide query -l ...$in | lines | where {|x| $x != $env.PWD}
       }
 
-      let multiple_completers = {{{!}}spans{{!}}
-          ## alias fixer start https://www.nushell.sh/cookbook/external_completers.html#alias-completions
+      let multiple_completers = {|spans|
+          # alias fixer start
           let expanded_alias = scope aliases
           | where name == $spans.0
           | get -o 0.expansion
@@ -49,18 +35,32 @@
           let spans = if $expanded_alias != null {
             $spans
             | skip 1
-            | prepend ($expanded_alias | split row ' ' {{!}} take 1)
+            | prepend ($expanded_alias | split row ' ' | take 1)
           } else {
             $spans
           }
-          ## alias fixer end
+          # alias fixer end
 
           match $spans.0 {
-            __zoxide_z {{!}} __zoxide_zi =&gt; $zoxide_completer
-            _ =&gt; $carapace_completer
-          } {{!}} do $in $spans
+            __zoxide_z | __zoxide_zi => $zoxide_completer
+            _ => $carapace_completer
+          } | do $in $spans
       }
-      }'';
+
+      # NOW set the completer to use the variable
+      $env.config.completions.external.completer = $multiple_completers
+
+      # Your yazi integration
+      def --env y [...args] {
+        let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+        yazi ...$args --cwd-file $tmp
+        let cwd = (open $tmp)
+        if $cwd != "" and $cwd != $env.PWD {
+          cd $cwd
+        }
+        rm -fp $tmp
+      }
+    '';
 
     shellAliases = {
       vi = "hx";
