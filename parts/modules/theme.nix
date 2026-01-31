9449584b8e4b73d3c7e58b121d6c7e3b696160a2
Dark_Loon <armandgaeln@gmail.com>
2026-01-31 22:20:32 +0100

    Convert wallpaper script to Nushell

--- parts/modules/theme.nix
+++ parts/modules/theme.nix
@@ -8,24 +8,11 @@ let
   cfg = config.myConfig.wallpaper;
 
   wallpaperScript = pkgs.writeScriptBin "wallpaper-cycler" ''
-    IMAGE_DIR="${cfg.imageDir}"
-
-    cd "$IMAGE_DIR"
-    while true; do
-        PID=$(pidof swaybg)
-        IMAGE_NUM=$(printf "%03d" $((($(date -u +%s) / 5400) % 16 + 1)))
-
-        if [ -f "./$IMAGE_NUM.png" ]; then
-            ${pkgs.swaybg}/bin/swaybg -i "$IMAGE_DIR/$IMAGE_NUM.png" -m fill &
-            sleep 1
-            kill $PID 2>/dev/null
-        fi
-        sleep 5399
-    done
+    #!${pkgs.nushell}/bin/nu
 
     def main [
       --interval: duration = 1.5hr
-      --num-images: int = 16
+      --num_images: int = 16
     ] {
       let image_dir = "${cfg.imageDir}"
 
@@ -33,24 +20,28 @@ let
       loop {
         let pid = (^pidof swaybg | complete | get stdout | str trim)
 
+        let current_time = (date now | format date "%s" | into int)
+        let interval_seconds = ($interval / 1sec) # Convert duration from nanoseconds to seconds
+        let division_result = ($current_time / $interval_seconds)
+        let mod_result = (($division_result | math floor) mod $num_images)
         let image_num = (
-          (date now | format date "%s" | into int)
-          / ($interval | into int)
-          | math floor
-          | $in mod $num-images
-          | $in + 1
+          ($mod_result + 1)
           | into string
           | fill --alignment right --character "0" --width 3
         )
 
-        let image_path = $"./($image_num).png"
+        #print $"Debug: current_time=($current_time), interval=($interval_seconds), division=($division_result), mod=($mod_result), image_num=($image_num)"
+
+        let image_path = $"($image_dir)/($image_num).png"
 
         if ($image_path | path exists) {
-        ^${pkgs.swaybg}/bin/swaybg -i $"($image_dir)/($image_num).png" -m fill &
-        sleep 1sec
+          do { ^${pkgs.swaybg}/bin/swaybg -i $image_path -m fill } | ignore
+          sleep 1sec
           if ($pid != "") {
-            ^kill $pid
+            try { ^kill $pid } catch { }
           }
+        } else {
+          print $"Image not found: ($image_path)"
         }
 
         sleep ($interval - 1sec)
@@ -90,7 +81,7 @@ in
       };
 
       Service = {
-        ExecStart = "${wallpaperScript} --interval ${cfg.interval} --num-images ${toString cfg.numImages}";
+        ExecStart = "${wallpaperScript}/bin/wallpaper-cycler --interval ${cfg.interval} --num_images ${toString cfg.numImages}";
         Restart = "on-failure";
       };
 
